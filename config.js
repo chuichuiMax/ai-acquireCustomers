e80ef91b5f228bc0d3836422e952c9f7846a59e8
chuichuiMax <1053340344@qq.com>
2026-09-15 09:44:41 +0800

    add

--- config.js
+++ config.js
@@ -1,8 +1,3 @@
-const LOCAL_BASE_URL = 'http://127.0.0.1:5050'
-const ONLINE_BASE_URL = 'https://ai.hi-run.net'
-
-const isDevelopment = process.env.NODE_ENV === 'development'
-
-export const BASE_URL = isDevelopment ? LOCAL_BASE_URL : ONLINE_BASE_URL
+export const BASE_URL = 'https://ai.hi-run.net'
 export const TOKEN_KEY = 'mp_token'
 export const SESSION_KEY = 'mp_session'
